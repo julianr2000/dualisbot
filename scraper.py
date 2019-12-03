3546c4eb5d1311ec3077f53d51b97499382ee8b8
Matthias Brehmer <m@brehmer-adf.de>
2019-12-03 21:57:22 +0100

    Use shorter function names

--- scraper.py
+++ scraper.py
@@ -17,7 +17,7 @@ def get_page(url):
         response.encoding = response.apparent_encoding
         return html.fromstring(response.text)
         
-def extract_metarefresh_tag_content(page):
+def get_mrefresh_content(page):
     """Get the link of the metarefresh tag if it exists"""
     tags = page.xpath('//meta[@http-equiv = "refresh"]/@content')
     if len(tags) > 1:
@@ -26,26 +26,26 @@ def extract_metarefresh_tag_content(page):
         return tags[0]
     return None
 
-def parse_metarefresh_tag_content(content):
+def mrefresh_to_relurl(content):
     """Get a relative url from the contents of a metarefresh tag"""
     urlstart = re.compile('.*URL=')
     _, url = content.split(';')
     url = urlstart.sub('', url)
     return url
 
-def url_relative_replace(relative_url, base_url):
+def relurl_to_url(relurl, base_url):
     """Append a page-relative url to a full url"""
-    relative = urlparse(relative_url)
+    relurl = urlparse(relurl)
     base_url = urlparse(base_url)
-    return urlunparse(base_url[:2] + relative[2:])
+    return urlunparse(base_url[:2] + relurl[2:])
 
-def get_metarefresh_url(page, base_url):
-    return url_relative_replace(parse_metarefresh_tag_content(extract_metarefresh_tag_content(page)), base_url)
+def get_mrefresh_url(page, url):
+    return relurl_to_url(mrefresh_to_relurl(get_mrefresh_content(page)), url)
 
-def follow_metarefesh_redir(url):
-    return get_metarefresh_url(get_page(url), url)
+def follow_mrefresh(url):
+    return get_mrefresh_url(get_page(url), url)
 
-def extract_form_data(page):
+def get_login_data(page):
     """Extract the path to the serverside script and the input form data from the Dualis login page"""
     form = page.xpath('//form[@id = "cn_loginForm"]')[0]
     inputs = form.xpath('.//input')
@@ -53,16 +53,17 @@ def extract_form_data(page):
     header.update({ 'usrname': data['secrets']['username'], 'pass': data['secrets']['password'] })
     return form.action, header
 
-def login(base_url):
-    partial_url, header = extract_form_data(get_page(base_url))
-    url = url_relative_replace(partial_url, base_url)
+def login(url):
+    relurl, header = get_login_data(get_page(url))
+    url = relurl_to_url(relurl, url)
     response = session.post(url, data=header)
-    login_url = url_relative_replace(parse_metarefresh_tag_content(response.headers['REFRESH']), base_url)
+    login_url = relurl_to_url(mrefresh_to_relurl(response.headers['REFRESH']), url)
     return login_url
 
 def main():
-    url = follow_metarefesh_redir(data['config']['url'])
-    url = follow_metarefesh_redir(url)
+    url = follow_mrefresh(data['config']['url']) # first redirect
+    url = follow_mrefresh(url) # second redirect
+    # we should be at the login page now
     url = login(url)
-    url = follow_metarefesh_redir(url)
+    url = follow_mrefresh(url) # redirect again...
     return get_page(url)
